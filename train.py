6b025eef536840dcbf7f45f08e553eaecd65ac2f
esillileu yun <esillileu@gmail.com>
2024-08-16 19:46:31 +0900

    #4 add train loop

--- train.py
+++ train.py
@@ -11,7 +11,20 @@ def train(img_list, gt_list, model, epoch, learning_rate, optimizer, criterion, 
         for iter in range(data_len):
             # [1] 빈칸을 작성하시오.
             # 학습 과정
-            
+
+            # 입력 데이터 준비
+            inputs = img_list[iter].to(model.device)  # 입력 이미지
+            labels = gt_list[iter].to(model.device)   # 정답 레이블
+
+            # 모델 예측값 얻기
+            outputs = model(inputs)
+
+            # 손실 계산
+            loss = criterion(outputs, labels)
+
+            # 역전파 및 가중치 업데이트
+            loss.backward()
+            optimizer.step()
 
             running_loss += loss.item()
             if (iter % 100 == 0) & (iter != 0):
