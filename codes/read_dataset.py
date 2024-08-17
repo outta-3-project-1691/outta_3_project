c7d02171c9f8c1e61c38a3b97b1eabb4174f74af
esillileu <esillileu@gmail.com>
2024-08-17 06:26:00 +0000

    close #12 add read_dataset_train_valid func

--- codes/read_dataset.py
+++ codes/read_dataset.py
@@ -16,3 +16,32 @@ def read_dataset(file_path, data_len):
             gt_list.append(np.array(data_gt[i]))
     
     return img_list, gt_list
+
+def read_dataset_train_valid(file_path, train_len, valid_len):
+    '''
+    지정된 train과 valid 셋을 반환
+    train_len+valid_len <= 10000 이도록 설정해야함
+
+    return:
+
+    '''
+    train_img_list = []
+    train_gt_list = []
+    valid_img_list = []
+    valid_gt_list = []
+    
+    with h5py.File(file_path, 'r') as file:
+        # 각 데이터셋 로드
+        data_img = file['ih']
+        data_gt = file['b_']
+        
+        # 길이만큼 데이터 불러오기
+        for i in range(train_len+valid_len):
+            if i < train_len:
+                train_img_list.append(np.array(data_img[i]))
+                train_gt_list.append(np.array(data_gt[i]))
+            else:
+                valid_img_list.append(np.array(data_img[i]))
+                valid_gt_list.append(np.array(data_gt[i]))
+        
+    return train_img_list, train_gt_list, valid_img_list , valid_gt_list
