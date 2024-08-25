1ab2debb51aba03e3814935cb6872c610b613b80
esillileu <esillileu@gmail.com>
2024-08-25 14:05:15 +0000

    add scheduler

--- codes/train.py
+++ codes/train.py
@@ -1,7 +1,10 @@
+import math
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import TensorDataset, DataLoader
+from torch.optim.lr_scheduler import _LRScheduler
 import numpy as np
 from tqdm.auto import tqdm
 
@@ -16,26 +19,38 @@ class SCLoss(nn.Module):
         super(SCLoss, self).__init__()
     
     def forward(self, pred, target):
-        # Pred와 Target은 (B, 1, H, W)의 형태라고 가정
-        # 그레이디언트 차이를 계산하기 위해 x와 y 방향으로 편미분
+        # Pred와 Target은 (B, 1, H, W)
         pred_dx = torch.abs(pred[:, :, :, :-1] - pred[:, :, :, 1:])
         pred_dy = torch.abs(pred[:, :, :-1, :] - pred[:, :, 1:, :])
         
         target_dx = torch.abs(target[:, :, :, :-1] - target[:, :, :, 1:])
         target_dy = torch.abs(target[:, :, :-1, :] - target[:, :, 1:, :])
         
-        # x와 y 방향의 그레이디언트 차이를 합산
         loss_x = F.l1_loss(pred_dx, target_dx)
         loss_y = F.l1_loss(pred_dy, target_dy)
         
         return loss_x + loss_y
 
+class SigmoidLRScheduler(_LRScheduler):
+    def __init__(self, optimizer, total_epochs, min_lr, max_lr, last_epoch=-1):
+        self.total_epochs = total_epochs
+        self.min_lr = min_lr
+        self.max_lr = max_lr
+        super(SigmoidLRScheduler, self).__init__(optimizer, last_epoch)
+    
+    def get_lr(self):
+        # 현재 진행된 epoch 비율
+        epoch_ratio = self.last_epoch / self.total_epochs
+        
+        # 시그모이드 함수를 통해 학습률 계산
+        sigmoid_factor = 1 / (1 + math.exp(-10 * (epoch_ratio - 0.5)))  # 시그모이드 함수
+        new_lr = self.min_lr + (self.max_lr - self.min_lr) * sigmoid_factor
+        
+        return [new_lr for _ in self.optimizer.param_groups]
 
 def epoch_loop(model, criterion, sc_loss, L, optimizer, dataloader, valid=False):
     epoch_loss = 0
-
     model.train()
-
     for inputs, targets in tqdm(dataloader, leave=False):
         inputs, targets = inputs.to(device=device), targets.to(device=device)
         targets = targets.long()
@@ -69,6 +84,7 @@ def train(img_list, gt_list, model, epoch, learning_rate, optimizer, criterion, 
     # 리스트를 텐서로 결합
     L = 0.5
     sc_loss = SCLoss()
+    scheduler = SigmoidLRScheduler(optimizer,epoch, learning_rate, 1e-8 )
     train_imgs = torch.tensor(np.array(img_list))
     train_gts = torch.tensor(np.array(gt_list)).squeeze(1)
     print()
@@ -81,6 +97,9 @@ def train(img_list, gt_list, model, epoch, learning_rate, optimizer, criterion, 
     model.train()
 
     for i in tqdm(range(epoch)):
-      epoch_loop(model, criterion, sc_loss, L,optimizer, train_loader)
+      epoch_loop(model, criterion, sc_loss, L, optimizer, train_loader)
       
+      print(scheduler.get_last_lr())
+      scheduler.step()
+
       torch.save(model.state_dict(), f'model_state_dict_epoch_{i}.pth')
